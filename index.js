3714d64ef6a84b7ee5180a373155ad0b8cce8671
Arancha Ferrero <aranchunfli11foz@hotmail.com>
2019-01-31 00:35:46 +0100

    Improved display design

--- index.js
+++ index.js
@@ -2,11 +2,37 @@
 let twitter = require('./requests/twitter')
 let azure = require('./requests/azure')
 
+const classify_score = (score) => {
+  if(score < 1/3) return 'NEGATIVE'
+  else if(score < 2/3) return 'NEUTRAL'
+  else return 'POSITIVE'
+}
+
+const display_results = (tweets, sentiments) => {
+
+  console.log("*************************************")
+  console.log("** LASTEST 15 TWEETS about #Brexit **")
+  console.log("*************************************\n\n")
+
+  tweets.map((tweet) => {
+    for(i in sentiments.documents) {
+      let sentiment = sentiments.documents[i]
+      if(tweet.id === sentiment.id){
+        let type = classify_score(sentiment.score)
+
+        console.log(tweet.text)
+        console.log(`--> ${type}`)
+        console.log(`Score: ${sentiment.score} (being 0 the worst and 1 the best))\n\n\n`)
+      }
+    }
+  })
+}
 
 async function main() {
   let tweets = await twitter.get_tweets()
-  let tweets_sentiments = await azure.get_sentiments(tweets)
-  console.log(tweets_sentiments)
+  let sentiments = await azure.get_sentiments(tweets)
+
+  display_results(tweets, sentiments)
 }
 
 main()
